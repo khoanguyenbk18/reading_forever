43dccb0fa36c9b43a07bc33ad7087d9344641fe2
khoanguyentaplifetechnology <khoa.nguyen@taplifetechnology.com>
2019-10-31 10:47:48 +0700

    Initial Deployment

--- src/server.js
+++ src/server.js
@@ -19,7 +19,16 @@ app.use(
 //routes
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/users', userRouter);
-const PORT = 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(process.env.PORT || PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
